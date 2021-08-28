8584a31b8966b2f9c33aadc16a6a5427158312a9
Yashika <yashika.lamba.phe19@itbhu.ac.in>
2021-08-28 16:13:48 +0530

    css linked

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.config['SECRET_KEY'] = 'IITBHU_Team6'
 @app.route('/')
 def filters():
     form = AddFilterForm()
-    # if form.is_submitted():
-    #     result = request.form
-    #     return render_template('addFilter.html', result = result)
+    if form.is_submitted():
+        result = request.form
+        return render_template('addFilter.html', result = result)
     return render_template('addFilter.html', form = form )
