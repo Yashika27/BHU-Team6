0befd3eeb43f9318042210a502d9c9384e033eb7
Yashika <yashika.lamba.phe19@itbhu.ac.in>
2021-08-28 15:29:01 +0530

    flask installed, filter form added

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+from forms import AddFilterForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'IITBHU_Team6'
+
+@app.route('/')
+def filters():
+    form = AddFilterForm()
+    # if form.is_submitted():
+    #     result = request.form
+    #     return render_template('addFilter.html', result = result)
+    return render_template('addFilter.html', form = form )
